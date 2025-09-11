50a2bc3eb7866578a707f58218a0f89191e15c23
Nofunny <isdoline999@gmail.com>
2025-09-11 19:25:59 +0900

    prime game v0.5

--- week05/src/PrimeGame.java
+++ week05/src/PrimeGame.java
@@ -3,7 +3,17 @@ import java.util.Scanner;
 public class PrimeGame {
     //숫자 하나를 넘겨 받아서 해당 수가 소수면 true 리턴, 소수가 아니면 false
     public static boolean isPrime(int n){
+        if(n <= 1){
+           return false;
+        }
+        for (int i = 2; i <=Math.sqrt(n); i = i + 1) {
+            if (n % i == 0) {
+                return false;
+                //break;
+            }
+            //System.out.print(i + " ");
 
+        }
 
         return true;
     }
@@ -15,22 +25,11 @@ public class PrimeGame {
         boolean isPrime = true;
         int number = sc.nextInt();
 
-        if(number <= 1){
-            isPrime = false;;
-        }
-
-
         if (isPrime) {
             System.out.println(number + " is a prime number~");
         }else{
-            for (int i = 2; i <=Math.sqrt(number); i = i + 1) {
-            if (number % i == 0) {
-                isPrime = false;
-                //break;
-            }
-            //System.out.print(i + " ");
 
-        }
+
             System.out.println(number + " is NOT prime number!");
         }
     }
