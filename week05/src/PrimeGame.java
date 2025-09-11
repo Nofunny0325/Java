0a7f634c45d41e2a1bdd9b229a64dda929f1c700
Nofunny <isdoline999@gmail.com>
2025-09-11 19:39:43 +0900

    prime number v0.6

--- week05/src/PrimeGame.java
+++ week05/src/PrimeGame.java
@@ -1,37 +1,39 @@
 import java.util.Scanner;
 
 public class PrimeGame {
-    //숫자 하나를 넘겨 받아서 해당 수가 소수면 true 리턴, 소수가 아니면 false
+    // 숫자 하나를 넘겨 받아서 해당 수가 소수면 true 리턴, 소수가 false
     public static boolean isPrime(int n){
         if(n <= 1){
-           return false;
-        }
-        for (int i = 2; i <=Math.sqrt(n); i = i + 1) {
-            if (n % i == 0) {
-                return false;
-                //break;
+            return false;
+        }else{
+            for(int i=2; i<=Math.sqrt(n); i=i+1){
+                if(n % i == 0){
+                    return false;
+                }
             }
-            //System.out.print(i + " ");
-
         }
-
         return true;
     }
 
     public static void main(String[] args) {
-        //System.out.println(Math.sqrt(25.0));
+        int[] numbers = {111, 977, 1, 2, 53, 1200};
+//
+//        for(int number : numbers){ //range bassed for
+//            if(isPrime(number)){
+//                System.out.println(number + " is a prime number~");
+//            }else{
+//                System.out.println(number + " is NOT prime number!");
+//            }
+//
+//        }// 배열
 
-        Scanner sc = new Scanner(System.in);
-        boolean isPrime = true;
-        int number = sc.nextInt();
+        for(int i=0; i<6; i++){
+            if(isPrime(numbers[i])){
+                System.out.println(numbers[i] + " is a prime number~");
+            }else{
+                System.out.println(numbers[i] + " is NOT prime number!");
+            }
 
-        if (isPrime) {
-            System.out.println(number + " is a prime number~");
-        }else{
-
-
-            System.out.println(number + " is NOT prime number!");
         }
     }
 }
-
