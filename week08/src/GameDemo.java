cabe7b900c3532628fb15871f164c02c07a72ac3
Nofunny0325 <isdoline999@gmail.com>
2025-10-30 21:31:18 +0900

    exception handling v0.7

--- week08/src/GameDemo.java
+++ week08/src/GameDemo.java
@@ -3,44 +3,45 @@ import java.util.Scanner;
 
 public class GameDemo {
     public static void main(String[] args) {
-//        int randomNumber = (int)(Math.random() * 3); // 0 ~ 2
+        //        int randomNumber = (int)(Math.random() * 3); // 0 ~ 2
 //        System.out.println(randomNumber);
 
         Pokemon playerPokemon = null;
         Scanner scanner = new Scanner(System.in);
 
-        try{
-            while (true){
-                System.out.print("플레이어 포켓몬스터 선택\n1) 피카츄  2) 꼬부기  3) 리자몽 : ");
-                int number = scanner.nextInt();  // string
-                if (number-1 == 0){
-                    playerPokemon = new Pikachu(100, 27, new NoFly());
-                    break;
-                }else if(number-1 == 1){
-                    playerPokemon = new Squirtle(120, 21, new NoFly());
-                    break;
-                }else if (number-1 == 2){
-                    playerPokemon = new Charizard(200, 40, new Wings());
-                    break;
-                }else
-                    System.out.println("메뉴에서 골라주세요");
+            while (true) {
+                try {
+                    System.out.print("플레이어 포켓몬스터 선택\n1) 피카츄  2) 꼬부기  3) 리자몽 : ");
+                    int number = scanner.nextInt(); // string
+                    if (number - 1 == 0) {
+                        playerPokemon = new Pikachu(100, 27, new NoFly());
+                        break;
+                    } else if (number - 1 == 1) {
+                        playerPokemon = new Squirtle(120, 21, new NoFly());
+                        break;
+
+                    } else if (number - 1 == 2) {
+                        playerPokemon = new Charizard(200, 40, new Wings());
+                        break;
+                    } else
+                        System.out.println("메뉴에서 골라주세요.");
+                } catch (InputMismatchException err) {
+                    System.out.println("숫자로 입력하세요. 메뉴에서 고르세요.");
+//                    System.out.println(err.getMessage());
+                    scanner.nextLine(); // 버퍼에 남아있는 값(여기선 문자열) 제거
+                }
             }
-        }catch (InputMismatchException err){
-            System.out.println("숫자로 입력하세요. 메뉴에서 고르세요.");
-            System.out.println(err.getMessage());
-        }
 
 
-        int randomNumber = (int)(Math.random() * 3); // 0 ~ 2
-        Pokemon enemyPokemon;
-        System.out.println("야생의 포켓몬스터 나타났습니다");
-        if(randomNumber == 0)
-            enemyPokemon = new Pikachu(100, 27, new NoFly());
-        else if (randomNumber == 1)
-            enemyPokemon = new Squirtle(120, 21, new NoFly());
-        else if (randomNumber == 2)
-            enemyPokemon = new Charizard(200, 40, new Wings());
-
+                int randomNumber = (int) (Math.random() * 3); // 0 ~ 2
+                Pokemon enemyPokemon;
+                System.out.println("야생의 포켓몬스터 나타났습니다");
+                if (randomNumber == 0)
+                    enemyPokemon = new Pikachu(100, 27, new NoFly());
+                else if (randomNumber == 1)
+                    enemyPokemon = new Squirtle(120, 21, new NoFly());
+                else if (randomNumber == 2)
+                    enemyPokemon = new Charizard(200, 40, new Wings());
 
 
 //        Pokemon playerPokemon = new Pikachu(100, 27, new NoFly());
@@ -85,5 +86,6 @@ public class GameDemo {
 //            turn++;
 //        }
 //        System.out.println("배틀 종료");
-    }
-}
+            }
+        }
+
